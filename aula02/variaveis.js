16097ab334a71f19164acd937c107b1ba55b41ce
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-02-20 08:30:37 -0300

    aula sobre variaveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,12 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEIsso = "?";
+
+console.log("tipo var: ",numero);
+numero=13;
+
+console.log(texto + " o número agora é "+numero)
+
+texto = 69
+
+console.log(texto+numero)
